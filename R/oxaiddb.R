2f657c20b1e07e626709b75d88aefbb6101e7b79
Sean Langcake <slangcake@bisoxfordeconomics.com.au>
2021-03-01 19:35:08 +1000

    Update oxaiddb.R

--- R/oxaiddb.R
+++ R/oxaiddb.R
@@ -1,27 +1,27 @@
 #'oxaiddb
 #'
-#'Loads in an AID database. 
+#'Loads in an AID database.
 #'@param db The database to load
 #'@param sect Character. The sector(s) you want to download variables for. Defaults to all
 #'@param vars Character. The variables to download. Defaults to all variables. Or use a custom selection.
 #'@param mod_dir Directory where your model software lives. Defaults to 'C:/AID'
 #'@return List with two objects: the data in long format ready for use with oxgraphs package & the h_end vector for automating forecast lines.
 #'These objects need to be renamed after you run this function
-#' @examples 
+#' @examples
 #' \donttest{x <- oxaiddb('Oct19.db') data <- x[[1]] h_end <- x[[2]]}
 #'@export
 
 oxaiddb <- function(db,sect=NULL,vars=NULL,mod_dir='C:/AID'){
-  
+
   a <- read_oedb(db,sector=sect,mnemonic=vars,model_dir=mod_dir,as_xts=0,fix_call=FALSE)
 
   data <- a$dat
   colnames(data)[1] = 'Dates'
   data <- data %>% melt(.,'Dates')
-  
+
   month(data$Dates) <- month(data$Dates)+2
   data$variable <- .revert_names(data$variable)
-  
+
   b <- a$last_hist %>% as.data.frame()
   colnames(b) <- 'hist_end'
   b$variable <- names(a$last_hist) %>% .revert_names()
@@ -31,12 +31,12 @@ oxaiddb <- function(db,sect=NULL,vars=NULL,mod_dir='C:/AID'){
   br <- b
   br$variable <- paste0(br$variable, " (RHS)")
   b <- rbind(b, br)
-  
+
   h_end <- select(b,variable,hist_end)
   x <- list(data,h_end)
-  
+
   return(x)
-  
+
 }
 
 
